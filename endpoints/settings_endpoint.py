f1d9615660fdedcaba5a7d6ff591f7f6abf8dcc5
Martijn de Vos <mdmartijndevos@gmail.com>
2016-04-27 20:32:53 +0200

    Implemented the retrieval of settings

--- endpoints/settings_endpoint.py
+++ endpoints/settings_endpoint.py
@@ -6,5 +6,49 @@ class SettingsEndpoint(resource.Resource):
 
     isLeaf = True
 
+    # Only contains the most necessary settings needed for the GUI
     def render_GET(self, request):
-        return json.dumps({"video": {"port": "1337"}})
+
+        settings_dict = {"settings": {
+            "general": {
+                "nickname": "Random nickname",
+                "minport": 1337,
+            },
+            "video": {
+                "enabled": True,
+                "port": "1337",
+            },
+            "libtorrent": {
+                "enabled": True,
+                "lt_proxytype": 0,
+                "lt_proxyserver": None,
+                "lt_proxyauth": None,
+                "utp": True,
+            },
+            "Tribler": {
+                "saveas": "/Users/tribleruser/downloads",
+                "showsaveas": 1,
+                "default_number_hops": 1,
+                "default_anonymity_enabled": True,
+                "default_safeseeding_enabled": True,
+                "maxuploadrate": 0,
+                "maxdownloadrate": 0,
+            },
+            "watch_folder": {
+                "enabled": True,
+                "watch_folder_dir": "/Users/tribleruser/watchfolder",
+            },
+            "downloadconfig": {
+                "seeding_mode": "ratio",
+                "seeding_time": 60,
+                "seeding_ratio": 2.0,
+            },
+            "multichain": {
+                "enabled": True,
+            },
+            "tunnel_community": {
+                "exitnode_enabled": True,
+            },
+        }}
+
+        return json.dumps(settings_dict)
